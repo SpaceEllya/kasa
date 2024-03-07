3a4dcb673d872f327b9ef195ab42137cd465ea70
Elina <ailie@192.168.1.2>
2024-03-07 21:32:06 +0100

    LKF FULL

--- backend/kfp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class KfpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kfp'
-
-    """def ready(self):
-        import kfp.signals"""
